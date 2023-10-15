95b3a67379729cea11708f2a12ba70603c663f32
nobleaustine <nobleaustine2001@gmail.com>
2023-10-15 20:09:33 +0200

    Updates

---
+++ precache-manifest.b0704e475f85143f557b9acb10af5ff6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c821ffc482487163a8fb",
+    "url": "/TON/static/css/main.c2801c64.chunk.css"
+  },
+  {
+    "revision": "c821ffc482487163a8fb",
+    "url": "/TON/static/js/main.6b9957be.chunk.js"
+  },
+  {
+    "revision": "b7fceb637cee163ca094",
+    "url": "/TON/static/js/runtime~main.99777a8a.js"
+  },
+  {
+    "revision": "701f9c09681a54277d32",
+    "url": "/TON/static/js/2.3eba1e21.chunk.js"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/TON/static/media/video.ed44dc5d.mp4"
+  },
+  {
+    "revision": "2b24408aa65851e0775214622e7d73f8",
+    "url": "/TON/static/media/turingMachine.2b24408a.svg"
+  },
+  {
+    "revision": "a5c7e5ba91f01eec82416bbf3f55da0f",
+    "url": "/TON/static/media/img(1).a5c7e5ba.svg"
+  },
+  {
+    "revision": "b0094ff2cddc31bb9d8a63ec3eba57a8",
+    "url": "/TON/static/media/img.b0094ff2.svg"
+  },
+  {
+    "revision": "e7ec751a49e0379aef25d12f7f2437ca",
+    "url": "/TON/static/media/img(2).e7ec751a.svg"
+  },
+  {
+    "revision": "0ebb5b96283f6c0b553d74c0fd9698ca",
+    "url": "/TON/index.html"
+  }
+];
